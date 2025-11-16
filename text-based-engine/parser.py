cd91a8770de0d1ae60139c2e2ad2772676a78c4a
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2025-11-15 23:59:26 -0800

    Create parser.py

---
+++ text-based-engine/parser.py
@@ -0,0 +1,31 @@
+def parseFile(file):
+    """
+    Splits a file into room text arrays.
+    """
+    rooms = []
+    current_room = []
+    for line in file:
+        if line == "---":
+            rooms.append(current_room)
+            current_room = []
+        else:
+            current_room.append(line)
+    if current_room:
+        rooms.append(current_room)
+    return rooms
+
+def parseText(file):
+    Splits a string into room text arrays.
+    """
+    rooms = []
+    current_room = []
+    for line in file.split("\n"):
+        if line == "---":
+            rooms.append(current_room)
+            current_room = []
+        else:
+            current_room.append(line)
+    if current_room:
+        rooms.append(current_room)
+    return rooms
+
