0f14cba30e86f3500581290d2000dd20557fe9ba
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2025-11-16 00:24:15 -0800

    Update parser.py

--- text-based-engine/parser.py
+++ text-based-engine/parser.py
@@ -1,4 +1,4 @@
-def parseFile(file):
+def parse_file(file):
     """
     Splits a file into room text arrays.
     """
@@ -10,11 +10,11 @@ def parseFile(file):
             current_room = []
         else:
             current_room.append(line)
-    if current_room:
+    if not all("" = line or line.isspace() for line in current_room):
         rooms.append(current_room)
     return rooms
 
-def parseText(file):
+def parse_text(file):
     """
     Splits a string into room text arrays.
     """
@@ -26,7 +26,24 @@ def parseText(file):
             current_room = []
         else:
             current_room.append(line)
-    if current_room:
+    if not all("" = line or line.isspace() for line in current_room):
         rooms.append(current_room)
+    return rooms
+
+def parse_room_body_list(room_body_list):
+    pass
+
+def parse_room_lists(room_lists):
+    rooms = {}
+    for current_room in room_lists:
+        title = current_room[0].strip()
+        room_id = current_room[1].strip()
+        if room_id[0] == "#":
+            room_id = room_id[1:].strip() # or just lstrip
+        room_body = parse_room_body_list(current_room[2:])
+        rooms[room_id] = {
+                "title": title,
+                "body": room_body,
+        }
     return rooms
 
