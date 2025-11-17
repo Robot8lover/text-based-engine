72e4b2dbc541c9988d6552744036d1c09a621eb8
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2025-11-16 16:31:57 -0800

    Update parser.py

--- text-based-engine/parser.py
+++ text-based-engine/parser.py
@@ -1,3 +1,5 @@
+import re
+
 def parse_file(file):
     """
     Splits a file into room text arrays.
@@ -31,7 +33,21 @@ def parse_text(file):
     return rooms
 
 def parse_room_body_list(room_body_list):
-    pass
+    body = {}
+    in_choices = False
+    pattern = r"\d+\.\s+"
+    for i, line in enumerate(room_body_list):
+        split = re.split(pattern, line)
+        if in_choices:
+            if len(split) == 1:
+                # Reached extra space after choices
+                break
+            body["choices"].append(split[1])
+        elif len(split) > 1:
+            body["text"] = "\n".join(room_body_list[:i])
+            body["choices"] = [split[1]]
+            in_choices = True
+    return body
 
 def parse_room_lists(room_lists):
     rooms = {}
@@ -39,11 +55,17 @@ def parse_room_lists(room_lists):
         title = current_room[0].strip()
         room_id = current_room[1].strip()
         if room_id[0] == "#":
+            # otherwise, we don't have an id maybe
+            # depending on our choices
+            # and then maybe we should assign it?
             room_id = room_id[1:].strip() # or just lstrip
         room_body = parse_room_body_list(current_room[2:])
-        rooms[room_id] = {
-                "title": title,
-                "body": room_body,
-        }
+        if room_id in rooms:
+            raise ValueError(f"Room id {room_id} is not unique")
+        else:
+            rooms[room_id] = {
+                    "title": title,
+                    "body": room_body,
+            }
     return rooms
 
