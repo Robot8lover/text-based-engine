1377d01b0e77531e20797cb5937122d69f669e4e
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2025-11-19 15:48:25 -0800

    Write choices_to_str

--- text-based-engine/runner.py
+++ text-based-engine/runner.py
@@ -1,5 +1,8 @@
-def choices_to_str():
-    pass
+def choices_to_str(choice_list: list) -> str:
+    s = ""
+    for i, choice in enumerate(choice_list):
+        s += f"{i+1}. {choice["text"]}\n"
+    return s
 
 def loop():
     pass
