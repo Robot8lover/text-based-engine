231a8ba15d3a60f11f200a27b44181821b6614b1
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2025-11-18 21:30:52 -0800

    Feat/file parsing (#4)
    * Set up basic structure
    
    * Create parser.py
    
    * Update parser.py
    
    * Update ideas
    
    * Update parser.py
    
    * Minor fixes to parser.py
    
    * Simplified parse_room_body_list
    
    * Add parse_choices to parser.py
    
    * Updated parse_choice
    
    * Add fill_titles
    
    * changed file to text in parse_text

--- ideas/main.md
+++ ideas/main.md
@@ -1,2 +1,6 @@
+- We might want to generate ids, either by having rooms added to the document
+  through a function/command line thing or by running through the document and
+  inserting them. The latter approach has certain advantages but you would need
+  to run the inserter every time you want to figure out how to link to a room.
 - array or dict to store each room:
     - \[prompt text, \[\[choice text, choice number/reference\],...\]\]
---
+++ text-based-engine/parser.py
@@ -0,0 +1,109 @@
+import re
+
+def parse_file(file) -> list:
+    """
+    Splits a file into room text arrays.
+    """
+    rooms = []
+    current_room = []
+    for line in file:
+        if line == "---":
+            rooms.append(current_room)
+            current_room = []
+        else:
+            current_room.append(line)
+    if not all("" == line or line.isspace() for line in current_room):
+        rooms.append(current_room)
+    return rooms
+
+def parse_text(text) -> list:
+    """
+    Splits a string into room text arrays.
+    """
+    rooms = []
+    current_room = []
+    for line in text.split("\n"):
+        if line == "---":
+            rooms.append(current_room)
+            current_room = []
+        else:
+            current_room.append(line)
+    if not all("" == line or line.isspace() for line in current_room):
+        rooms.append(current_room)
+    return rooms
+
+def parse_choice(choice: str) -> dict:
+    # Test if it is [text](#id) or (#id)
+    # If the latter, text is the title of the room
+    # Except that requires later knowledge hmmm.
+    # So maybe we should make a later pass
+    text_pattern = r"\[([\w ()[\]-]*)\](\(#([\w-]*)\))"
+    id_pattern = r"(\(#([\w-]*)\))"
+    choice = choice.strip()
+    id_match = re.match(id_pattern, choice)
+    choice_dict = {
+        "text": None,
+        "id": None,
+    }
+    if id_match:
+        choice_dict["id"] = id_match.group(1)
+    else:
+        text_match = re.match(text_pattern, choice)
+        if text_match:
+            choice_dict["text"], choice_dict["id"] = text_match.group(1, 2)
+            # or .groups()
+        else:
+            raise ValueError(f"Choice `{choice}` did not match expected patterns.")
+    return choice_dict
+
+def parse_choices_list(choices_list):
+    choices = []
+    pattern = r"\d+\.\s+"
+    for line in choices_list:
+        split = re.split(pattern, line)
+        if len(split) == 1:
+            # Reached extra line after choices
+            # For now, we assume that this line must be unimportant (empty)
+            break
+        # We do not check for proper numbering
+        choices.append(parse_choice(split[1]))
+    return choices
+
+def parse_room_body_list(room_body_list):
+    body = {}
+    pattern = r"\d+\.\s+"
+    for i, line in enumerate(room_body_list):
+        if re.match(pattern, line):
+            body["text"] = "\n".join(room_body_list[:i])
+            body["choices"] = parse_choices_list(room_body_list[i:])
+            break
+    return body
+
+def parse_room_lists(room_lists):
+    rooms = {}
+    for current_room in room_lists:
+        title = current_room[0].strip()
+        room_id = current_room[1].strip()
+        if room_id[0] == "#":
+            # otherwise, we don't have an id maybe
+            # depending on our choices
+            # and then maybe we should assign it?
+            room_id = room_id[1:].strip() # or just lstrip
+        room_body = parse_room_body_list(current_room[2:])
+        if room_id in rooms:
+            raise ValueError(f"Room id {room_id} is not unique")
+        else:
+            rooms[room_id] = {
+                "title": title,
+                "body": room_body,
+            }
+    return rooms
+
+def fill_titles(room_list):
+    """
+    Modifies the names of rooms in the choices.
+    """
+    for room in room_list:
+        for i, choice in enumerate(room["body"]["choices"]):
+            if choice["text"] is None:
+                choice["text"] = room_list[choice["id"]]["title"]
