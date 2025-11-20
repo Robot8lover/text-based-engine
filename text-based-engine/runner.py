40997f8e61c6d812be3aea71aafed4a85728c3ee
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2025-11-19 16:14:09 -0800

    Update runner.py
    Updated loop

--- text-based-engine/runner.py
+++ text-based-engine/runner.py
@@ -1,8 +1,21 @@
+from user_input import get_choice_input
+
 def choices_to_str(choice_list: list) -> str:
     return "\n".join(f"{i+1}. {choice["text"]}" for i, choice in enumerate(choice_list))
 
-def loop():
-    pass
+def loop(rooms, start_id):
+    current_id = start_id
+    while True:
+        current_room = rooms[current_id]
+        room_body = current_room["body"]
+        choices = room_body["choices"]
+        print(current_room["title"])
+        print()
+        print(room_body["text"])
+        print()
+        print(choices_to_str(choices))
+        next_index = get_choice_input(len(choices))
+        current_id = choices[next_index]["id"]
 
 def run():
     pass
