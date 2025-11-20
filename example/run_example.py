9f6b6075f0bdac7c96aa628240962354cd8d9fc4
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2025-11-19 16:58:29 -0800

    Update run_example.py

--- example/run_example.py
+++ example/run_example.py
@@ -1,7 +1,9 @@
 import sys
-sys.path.append("../text_based_engine")
-
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 from text_based_engine.runner import run
 
 if __name__ == "__main__":
-    run("./example.txt")
+    run("example/example.txt")
